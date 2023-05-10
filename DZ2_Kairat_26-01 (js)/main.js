538444f73a0964987e2fe6867b1c095bcf56531a
Kairat <132819656+taalaibek0vk@users.noreply.github.com>
2023-05-10 07:20:45 +0600

    Выполнил DZ 2

---
+++ DZ2_Kairat_26-01 (js)/main.js
@@ -0,0 +1,57 @@
+// 1.
+// var arr = [10, 20, 30, 50, 235, 3000, 5000439857]
+
+// for (var i = 0; i < arr.length; i++) {
+//   var num = String(arr[i])
+//   if (num[0] === '1' || num[0] === '2' || num[0] === '5') {
+//     console.log(arr[i])
+//   }
+// }
+
+//2.
+// for (var index = 20; index >= 0; index--) {
+//   console.log(index);
+// }
+
+
+
+
+//3.
+// var svetofor = String(prompt('цветы'))
+
+// var object = {
+//   red: 'красный',
+//   yellow: 'желтый',
+//   green: 'зеленый'
+// }
+
+// if (svetofor === object.red) alert('Стоп!')
+// else if (svetofor === object.yellow) alert('Приготовься!')
+// else if (svetofor === object.green) alert('Вперед!')
+// else alert('Ошибка!')
+
+
+// switch (svetofor) {
+//   case object.red:
+//     alert('Стоп!')
+//     break;
+//   case object.yellow:
+//     alert('Приготовься!')
+//     break;
+//   case object.green:
+//     alert('Вперед!')
+//     break;
+//   default:
+//     alert('Ошибка!')
+//     break;
+// }
+
+
+
+
+//4.
+// var arr = []
+// for (var count = 0; count <= 10; count++) {
+//   // arr.push(count)
+// }
+// console.log(arr);

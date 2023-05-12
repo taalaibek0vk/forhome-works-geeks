06ed52bc46180bb8093c474722557ddd5533e676
Kairat <132819656+taalaibek0vk@users.noreply.github.com>
2023-05-12 18:07:39 +0600

    Сделал HW3

---
+++ DZ3_Kairat_26-01 (js)/main.js
@@ -0,0 +1,49 @@
+// 1.
+// function getDataType() {
+//   var result = 42
+//   return console.log(typeof result);
+// }
+// getDataType()
+
+// var getDataType = () => {
+//   var result = 'Hello World'
+//   return console.log(typeof result)
+// }
+// getDataType()
+
+// var getDataType = () => {
+//   var result = false
+//   return console.log(typeof result)
+// }
+// getDataType()
+
+
+
+// 2.
+// function sum(a, b) {
+//   return console.log(40 + 2)
+// }
+// sum()
+
+// function sub(a, b) {
+//   return console.log(52 - 12);
+// }
+// sub()
+
+// function multiply(a, b) {
+//   return console.log(23 * 10);
+// }
+// multiply()
+
+// function division(a, b) {
+//   return console.log(30 / 3);
+// }
+// division()
+
+// 3.
+// var array = [-1, 0, 5, 7, 15, 26]
+
+// var result = array.find(function () {
+//   return console.log(array.indexOf(5));
+// })
+// array()

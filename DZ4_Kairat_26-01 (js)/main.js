1ffcc5bcdcaea9da9d4024145f2c9c46b6cd01b6
Kairat <132819656+taalaibek0vk@users.noreply.github.com>
2023-05-20 13:47:23 +0600

    Выполнил HW4

---
+++ DZ4_Kairat_26-01 (js)/main.js
@@ -0,0 +1,38 @@
+// 1-е задание
+// var counter = 0
+// function increment() {
+//   counter++
+//   document.getElementById('counter').innerHTML = counter
+//   document.getElementById('counter').style.color = 'green'
+// }
+// function decrement() {
+//   if (counter > 0) {
+//     counter--
+//     document.getElementById('counter').innerHTML = counter
+//     document.getElementById('counter').style.color = 'red'
+//   }
+// }
+
+
+// 2-е задание
+// function text() {
+//   var anyText = prompt('Введите текст');
+//   document.querySelector("#text").innerHTML = anyText;
+// }
+
+
+//3-е задание
+// var svetofor = String(prompt('Смотри на сфетофор!'))
+
+// if (svetofor === 'Красный') {
+//   document.querySelector('.box_1').style.backgroundColor = 'red'
+//   alert('STOP!')
+// } else if (svetofor === 'Желтый') {
+//   document.querySelector('.box_2').style.backgroundColor = 'yellow'
+//   alert('BE READY!')
+// } else if (svetofor === 'Зеленый') {
+//   document.querySelector('.box_3').style.backgroundColor = 'green'
+//   alert('GO!')
+// } else {
+//   alert('ВВЕДИ ЦВЕТ!')
+// }
